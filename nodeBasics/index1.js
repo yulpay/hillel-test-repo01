ea247712b752dc41f8e0ae9827d67c62bb2750b0
yulpay <yuyupayul@gmail.com>
2026-07-24 12:34:00 +0200

    homework lesson 4

---
+++ nodeBasics/index1.js
@@ -0,0 +1,14 @@
+const c = `JS`
+let a = `Hello ${c}`;
+const b = `World`;
+
+console.log(`${a} ${b}!`);
+
+let number = `fvf`;
+console.log(number );
+
+let isTrue = true;
+let isFalse = false;
+
+console.log(5 > 10);
+console.log(5 < 10);
